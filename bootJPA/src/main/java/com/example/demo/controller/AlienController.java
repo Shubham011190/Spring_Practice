d76f36f5b711e214b268d65b30c2b4e7ff503412
Shubham Singhal <shubham.011190@gmail.com>
2021-10-26 17:41:47 +0530

    REST basic

--- bootJPA/src/main/java/com/example/demo/controller/AlienController.java
+++ bootJPA/src/main/java/com/example/demo/controller/AlienController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.alienDao;
@@ -26,15 +28,41 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@RequestMapping("/getAlien")
-		public ModelAndView getAlien(@RequestParam int aid) {
-		ModelAndView mv = new ModelAndView("getAlien.jsp");
-		Alien alien = aliend.findById(aid).orElse(new Alien());
-		mv.addObject(alien);
+//	@RequestMapping("/getAlien")
+//		public ModelAndView getAlien(@RequestParam int aid) {
+//		ModelAndView mv = new ModelAndView("getAlien.jsp");
+//		Alien alien = aliend.findById(aid).orElse(new Alien());
+//		mv.addObject(alien);
+//		
+//		System.out.println(aliend.findByTech("Java"));
+//		System.out.println(aliend.findByAidGreaterThan(102));
+//		System.out.println(aliend.findByTechSortedName("Java"));
+//		return mv;
+//		}
+	
+	@RequestMapping("/aliens")
+	@ResponseBody
+	public String getAliens() {
 		
-		System.out.println(aliend.findByTech("Java"));
-		System.out.println(aliend.findByAidGreaterThan(102));
-		System.out.println(aliend.findByTechSortedName("Java"));
-		return mv;
-		}
+		return aliend.findAll().toString();
+	}
+	
+	
+	@RequestMapping("/aliens/{aid}")
+	@ResponseBody
+	public String getAlien(@PathVariable("aid") int aid) {
+		
+		return aliend.findById(aid).toString();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
